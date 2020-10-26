a485c5f2c5eb33fcd4f002cb1316abc34f2c5c1c
Queen <queensform@queens-air.attlocal.net>
2020-10-26 13:18:53 -0700

    MainController articles mapping restTemplate syntax correction

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -83,7 +83,7 @@ public class MainController {
 	// 	userRepository.save(n);
 	// 	return "Saved";
 	// }
-	return restTemplate.getForObject(url, String.class).articles;
+	return restTemplate.getForObject(url, String.class);
 	}
 	
 
